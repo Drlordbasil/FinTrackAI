87a8b7ead4089f13067244a1d987ccfa923f1467
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-08-21 08:39:28 -0400

    Initial commit - AI_CoPilot.py

---
+++ AI_CoPilot.py
@@ -0,0 +1,37 @@
+Sure! Here's a chatbot script that helps the user run the provided `main.py` program locally on their PC:
+
+```python
+import subprocess
+
+def run_program():
+    try:
+        subprocess.run(["python", "main.py"], check=True)
+    except subprocess.CalledProcessError:
+        print("An error occurred while running the program.")
+
+def chatbot():
+    print("Welcome to the Expense Tracker Chatbot!")
+    print("How can I assist you today?")
+
+    while True:
+        user_input = input("> ")
+
+        if user_input.lower() == "run program":
+            print("Running the program...")
+            run_program()
+            print("Program execution completed.")
+        elif user_input.lower() == "exit":
+            print("Thank you for using the chatbot. Goodbye!")
+            break
+        else:
+            print("Sorry, I didn't understand that. Please try again.")
+
+if __name__ == "__main__":
+    chatbot()
+```
+
+To use the chatbot, simply run the script and interact with the chatbot. When prompted, type "run program" to execute the `main.py` program, or type "exit" to exit the chatbot.
+
+Note: Make sure the `main.py` file is in the same directory as the chatbot script before running.
+
+I hope this helps! Let me know if you have any further questions.
